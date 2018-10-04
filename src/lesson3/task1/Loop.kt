50c947916a47b4b87a949b90a3798c422d49d0f7
Keita18 <askeita@yahoo.com>
2018-10-04 23:01:03 +0300

    lesson2\task1\resolved

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -1,6 +1,9 @@
 @file:Suppress("UNUSED_PARAMETER")
 package lesson3.task1
 
+import lesson1.task1.sqr
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.math.sqrt
 
 /**
@@ -153,7 +156,14 @@ fun maxDivisor(n: Int): Int {
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean = TODO()
+fun isCoPrime(m: Int, n: Int): Boolean {
+    if (n < 2 || m < 2) return false
+    for (k in 2..sqrt(n.toDouble()).toInt() * sqrt(m.toDouble()).toInt()) {
+        if (m % k == 0 && n % k == 0)
+            return false
+    }
+    return true
+}
 
 /**
  * Простая
@@ -162,7 +172,15 @@ fun isCoPrime(m: Int, n: Int): Boolean = TODO()
  * то есть, существует ли такое целое k, что m <= k*k <= n.
  * Например, для интервала 21..28 21 <= 5*5 <= 28, а для интервала 51..61 квадрата не существует.
  */
-fun squareBetweenExists(m: Int, n: Int): Boolean = TODO()
+fun squareBetweenExists(m: Int, n: Int): Boolean {
+    if (m == 1 && n == 1) return true
+    for (k in min(m , n)..max(m, n)) {
+        if (k*k <= max(m, n) && k*k >= min(m, n))
+            break
+        return true
+    }
+    return false
+}
 
 /**
  * Средняя
